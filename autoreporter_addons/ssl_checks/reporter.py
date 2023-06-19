0b431a15d752228be11dd5f43997e2d6f81eaf30
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-19 10:42:05 +0200

    Cleaning up the code by extracting vars

--- autoreporter_addons/ssl_checks/reporter.py
+++ autoreporter_addons/ssl_checks/reporter.py
@@ -47,14 +47,17 @@ class SSLChecksReporter(Reporter):  # type: ignore
         if task_result["headers"]["receiver"] != "ssl_checks":
             return []
 
-        domain = task_result["payload"]["domain"]
+        result = task_result["result"]
+        payload = task_result["payload"]
+
+        domain = payload["domain"]
         domain_parts = [part for part in domain.split(".") if part]
         # We do the filtering both in the scanning module and in the reporter so that after changing this setting
         # the user wouldn't need to wait for the next scan for the configuration to take effect.
         if domain_parts[0] in ExtraModulesConfig.SUBDOMAINS_TO_SKIP_SSL_CHECKS:
             return []
 
-        if not isinstance(task_result["result"], dict):
+        if not isinstance(result, dict):
             return []
 
         try:
@@ -68,9 +71,9 @@ class SSLChecksReporter(Reporter):  # type: ignore
         except Exception:
             logger.warning(f"Unable to check whether domain {domain} has identical content to parent domain")
 
-        if "response_status_code" in task_result["result"] and "response_content_prefix" in task_result["result"]:
-            response_status_code = task_result["result"]["response_status_code"]
-            response_content_prefix = task_result["result"]["response_content_prefix"]
+        if "response_status_code" in result and "response_content_prefix" in result:
+            response_status_code = result["response_status_code"]
+            response_content_prefix = result["response_content_prefix"]
 
             if (
                 # We don't filter out all 4xx codes - for example, we want to show a message for
@@ -88,24 +91,24 @@ class SSLChecksReporter(Reporter):  # type: ignore
 
             # We have systems in our constituency where a redirect is performed from http://domain to a different domain and https://domain is not used
             # and is misconfigured/the certificate expires. For now let's treat this as a false positive.
-            if not task_result["result"].get("bad_redirect", False) and "redirect_url" in task_result["result"]:
-                redirect_url_parsed = urllib.parse.urlparse(task_result["result"]["redirect_url"])
-                if redirect_url_parsed.hostname != task_result["payload"]["domain"]:
+            if not result.get("bad_redirect", False) and "redirect_url" in result:
+                redirect_url_parsed = urllib.parse.urlparse(result["redirect_url"])
+                if redirect_url_parsed.hostname != payload["domain"]:
                     return []
 
         result = []
-        if task_result["result"].get("certificate_authority_invalid", False):
+        if result.get("certificate_authority_invalid", False):
             result.append(
                 Report(
                     top_level_target=get_top_level_target(task_result),
-                    target=f'https://{task_result["payload"]["domain"]}:443/',
+                    target=f'https://{payload["domain"]}:443/',
                     report_type=SSLChecksReporter.CERTIFICATE_AUTHORITY_INVALID,
                     additional_data={},
                     timestamp=task_result["created_at"],
                 )
             )
-        if task_result["result"].get("bad_redirect", False):
-            response_content_prefix = task_result["result"].get("response_content_prefix", "")
+        if result.get("bad_redirect", False):
+            response_content_prefix = result.get("response_content_prefix", "")
             if not any(
                 [fragment in response_content_prefix for fragment in FILTERED_WEBSITE_FRAGMENTS_FOR_BAD_REDIRECT]
             ):
@@ -115,53 +118,55 @@ class SSLChecksReporter(Reporter):  # type: ignore
                 try:
                     soup = BeautifulSoup(response_content_prefix.lower(), "html.parser")
                 except Exception:  # parsing errors
-                    logger.exception("Unable to parse HTML from %s", task_result["payload"]["domain"])
+                    logger.exception("Unable to parse HTML from %s", payload["domain"])
                     soup = None
 
                 if not soup or not soup.find_all("meta", attrs={"http-equiv": "refresh"}):
                     result.append(
                         Report(
                             top_level_target=get_top_level_target(task_result),
-                            target=f'http://{task_result["payload"]["domain"]}:80/',
+                            target=f'http://{payload["domain"]}:80/',
                             report_type=SSLChecksReporter.NO_HTTPS_REDIRECT,
                             additional_data={},
                             timestamp=task_result["created_at"],
                         )
                     )
 
-        if task_result["result"].get("cn_different_from_hostname", False):
+        if result.get("cn_different_from_hostname", False):
             # If the domain starts with www. but the version without www. is in the names list,
             # let's assume just the version without www. is advertised and used by the users.
             if not (
-                task_result["payload"]["domain"].startswith("www.")
-                and task_result["payload"]["domain"][4:] in task_result["result"]["names"]
+                payload["domain"].startswith("www.")
+                and payload["domain"][4:] in result["names"]
             ):
+                names_string = ", ".join(sorted(set(result["names"])))
+
                 result.append(
                     Report(
                         top_level_target=get_top_level_target(task_result),
-                        target=f'https://{task_result["payload"]["domain"]}:443/',
+                        target=f'https://{payload["domain"]}:443/',
                         report_type=SSLChecksReporter.BAD_CERTIFICATE_NAMES,
-                        additional_data={"names_string": ", ".join(sorted(set(task_result["result"]["names"])))},
+                        additional_data={"names_string": names_string},
                         timestamp=task_result["created_at"],
                     )
                 )
-        if task_result["result"].get("almost_expired", False):
+        if result.get("almost_expired", False):
             result.append(
                 Report(
                     top_level_target=get_top_level_target(task_result),
-                    target=f'https://{task_result["payload"]["domain"]}:443/',
+                    target=f'https://{payload["domain"]}:443/',
                     report_type=SSLChecksReporter.ALMOST_EXPIRED_SSL_CERTIFICATE,
-                    additional_data={"expiry_date": task_result["result"]["expiry_date"]},
+                    additional_data={"expiry_date": result["expiry_date"]},
                     timestamp=task_result["created_at"],
                 )
             )
-        if task_result["result"].get("expired", False):
+        if result.get("expired", False):
             result.append(
                 Report(
                     top_level_target=get_top_level_target(task_result),
-                    target=f'https://{task_result["payload"]["domain"]}:443/',
+                    target=f'https://{payload["domain"]}:443/',
                     report_type=SSLChecksReporter.EXPIRED_SSL_CERTIFICATE,
-                    additional_data={"expiry_date": task_result["result"]["expiry_date"]},
+                    additional_data={"expiry_date": result["expiry_date"]},
                     timestamp=task_result["created_at"],
                 )
             )
