7cbe11c773dfb98a216063cc1fe7e93b8228a9ea
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-11-01 19:13:01 +0100

    .

---
+++ test/data/sql_injection_mysql_clean_urls/index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <a href="/not_vuln/1">Article 1</a>
+        <a href="/not_vuln/2">Article 2</a>
+        <a href="/not_vuln/3">Article 3</a>
+        <a href="/vuln/4">Article 4</a>
+        <a href="/not_vuln/5">Article 5</a>
+    </body>
+</html>
