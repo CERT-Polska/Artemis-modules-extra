27dd665dcc5e7e8a0e53af674ec9051af66b98fa
Matie26 <matb20000@gmail.com>
2024-06-27 12:22:29 +0200

    add support for services without ssl

--- forti_vuln/forti_vuln.py
+++ forti_vuln/forti_vuln.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import socket
 import ssl
-from typing import Tuple
+from typing import Optional, Tuple
 
 from artemis.binds import Device, TaskStatus, TaskType
 from artemis.config import Config
@@ -30,7 +30,7 @@ Transfer-Encoding: chunked\r
 """
 
 
-class FortiVuln(ArtemisBase):  # type: ignore[misc]
+class FortiVuln(ArtemisBase):  # type: ignore
     """
     Checks FortiOS instance for CVE-2024-21762 vulnerability
     source: https://github.com/BishopFox/CVE-2024-21762-check
@@ -42,39 +42,51 @@ class FortiVuln(ArtemisBase):  # type: ignore[misc]
     ]
 
     @staticmethod
-    def _send_req(context: ssl.SSLContext, address: Tuple[str, int], req: bytes) -> int:
+    def _send_req(ssl_context: Optional[ssl.SSLContext], address: Tuple[str, int], req: bytes) -> int:
         try:
             s = socket.create_connection(address, timeout=5)
         except Exception:
             return -1
-        ss = context.wrap_socket(s)
-        ss.send(req)
+
+        if ssl_context is not None:
+            ss = ssl_context.wrap_socket(s)
+            ss.send(req)
+        else:
+            s.send(req)
+
         try:
-            ss.read(2048)
+            if ssl_context is not None:
+                ss.read(2048)
+            else:
+                s.recv(2048)
             return 1
         except socket.timeout:
             return 0
 
-    def vuln_check(self, host: str, port: int) -> int:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
+    def vuln_check(self, host: str, port: int, is_ssl: bool) -> int:
+        ssl_context = None
+        if is_ssl:
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
 
         http_host = f"{host}:{port}"
         self.log.info(f"forti vuln scanning {http_host}")
 
-        user_agent = ""
+        user_agent = "FortiOS vulnerability scanner"
         if Config.Miscellaneous.CUSTOM_USER_AGENT:
             user_agent = Config.Miscellaneous.CUSTOM_USER_AGENT
 
         r1 = throttle_request(
-            lambda: self._send_req(context, (host, port), CONTROL_REQUEST.format(http_host, user_agent).encode())
+            lambda: self._send_req(ssl_context, (host, port), CONTROL_REQUEST.format(http_host, user_agent).encode())
         )
         if r1 in [-1, 0]:
             return -1
         else:
             r2 = throttle_request(
-                lambda: self._send_req(context, (host, port), VULN_CHECK_REQUEST.format(http_host, user_agent).encode())
+                lambda: self._send_req(
+                    ssl_context, (host, port), VULN_CHECK_REQUEST.format(http_host, user_agent).encode()
+                )
             )
             if r2 == 0:
                 return 1
@@ -85,21 +97,18 @@ class FortiVuln(ArtemisBase):  # type: ignore[misc]
         status = TaskStatus.OK
         status_reason = None
 
-        if current_task.get_payload("ssl"):
-            host = get_target_host(current_task)
-            port = current_task.get_payload("port")
-            check = self.vuln_check(host, port)
+        ssl = current_task.get_payload("ssl")
+        host = get_target_host(current_task)
+        port = current_task.get_payload("port")
+        check = self.vuln_check(host, port, ssl)
 
-            if check == -1:
-                status = TaskStatus.ERROR
-                status_reason = "Could not send control request"
-            elif check == 1:
-                result.append("CVE-2024-21762")
-                status = TaskStatus.INTERESTING
-                status_reason = "Detected CVE-2024-21762 vulnerability"
-        else:
+        if check == -1:
             status = TaskStatus.ERROR
-            status_reason = "Omitted because service does not use ssl encryption"
+            status_reason = "Could not send control request"
+        elif check == 1:
+            result.append("CVE-2024-21762")
+            status = TaskStatus.INTERESTING
+            status_reason = "Detected CVE-2024-21762 vulnerability"
 
         self.db.save_task_result(task=current_task, status=status, status_reason=status_reason, data=result)
 
