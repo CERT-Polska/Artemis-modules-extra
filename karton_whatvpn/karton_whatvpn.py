ee8f1572890f671c9372d3c3e54280cc8eff2b7f
Łukasz Gruszczelak <lukasz.gruszczelak@cert.pl>
2024-08-12 17:34:17 +0200

    Return OK/INTERESTING statuses

--- karton_whatvpn/karton_whatvpn.py
+++ karton_whatvpn/karton_whatvpn.py
@@ -5,6 +5,7 @@ from artemis.binds import TaskStatus, TaskType, Service
 from artemis.module_base import ArtemisBase
 from artemis.task_utils import get_target_url, get_target_host
 from karton.core import Task
+import string
 
 
 from extra_modules_config import ExtraModulesConfig
@@ -18,41 +19,45 @@ class WhatVPN(ArtemisBase):
     
     identity = "what-vpn"
     filters = [
-        # {"type": TaskType.IP.value}, #czy service:http nie zawiera też IPków?
+        {"type": TaskType.IP.value}, 
         {"type": TaskType.DOMAIN.value}
         # {"type": TaskType.SERVICE.value, "service": Service.HTTP.value}
     ]
 
-    def run(self, current_task: Task) -> None:
-        target_host = get_target_host(current_task)
-        
-        logger.info("Requested to check if %s is a VPN gateway", target_host)
-        data = subprocess.run( #print?
+    def _process(self, current_task: Task, host: str) -> None:
+        output = subprocess.run( #print?
             [
                 "what-vpn",
-                target_host
+                host
 
             ],
             capture_output=True
         )
+        output = output.stdout.decode("utf-8")
+        detected_vpn = output.split(' ', 1)[1] 
 
-        status = TaskStatus.OK
-        status_reason = None
-        result = data.stdout.decode("utf-8")
-        if "no match" in result:
-            result = {"error": "Could not identify a VPN gateway"}
+        if "no match" in detected_vpn:
+            status = TaskStatus.OK
+            status_reason = "Could not identify a VPN gateway"
         else:
-            result = result.split(' ', 1)[1]
             status = TaskStatus.INTERESTING
-            status_reason = f"Detected {result}"
+            status_reason = f"Detected {detected_vpn}"
 
         # Save the task result to the database
         self.db.save_task_result(
             task=current_task,
             status = status,
             status_reason=status_reason,
-            data={"result": result},
+            data=detected_vpn,
         )
+
+    def run(self, current_task: Task) -> None:
+        target_host = get_target_host(current_task)
+        
+        self.log.info("Requested to check if %s is a VPN gateway", target_host)
+
+        self._process(current_task, target_host)
+
 
 if __name__ == "__main__":
     WhatVPN().loop()
