690ab25611a9a62612f44855044af565f750918c
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-09-19 08:49:27 +0200

    Tuning sqlmap config

--- extra_modules_config.py
+++ extra_modules_config.py
@@ -45,7 +45,7 @@ class ExtraModulesConfig:
     SQLMAP_TAMPER_SCRIPTS = decouple.config(
         "SQLMAP_TAMPER_SCRIPTS",
         cast=decouple.Csv(str),
-        default="",
+        default=",".join(["chardoubleencode"]),
     )
 
     # Timeout counted in seconds, after which the what-vpn module terminates a connection and starts using the next sniffer.
