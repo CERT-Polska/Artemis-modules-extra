f52e807bafe3f555e4c85f19aa07559e39f1bba8
kazet <krzysztof.zajac@cert.pl>
2025-06-18 09:28:19 +0200

    Increasing sqlmap level (#204)

--- extra_modules_config.py
+++ extra_modules_config.py
@@ -41,6 +41,8 @@ class ExtraModulesConfig:
         cast=decouple.Csv(str),
         default=",".join(
             [
+                "--level",
+                "2",
                 "--technique",
                 "BU",
                 "--skip-waf",
