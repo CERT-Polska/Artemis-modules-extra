042ae068f1772e883e70998811aae16d518d7488
kazet <krzysztof.zajac@cert.pl>
2025-07-11 12:18:22 +0200

    Whatvpn reporter (#208)

---
+++ autoreporter_addons/whatvpn/reporter.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict, List
+
+from artemis.reporting.base.asset import Asset
+from artemis.reporting.base.asset_type import AssetType
+from artemis.reporting.base.reporter import Reporter
+
+
+class WhatVPNRreporter(Reporter):  # type: ignore
+    @staticmethod
+    def get_assets(task_result: Dict[str, Any]) -> List[Asset]:
+        if task_result["headers"]["receiver"] != "what-vpn":
+            return []
+
+        return [
+            Asset(
+                asset_type=AssetType.VPN,
+                name=task_result["target_string"],
+                additional_type=task_result["result"].strip(),
+            )
+        ]
